8a187f710d081319268af6915932614654509bd5
inshalayaz <inshal.ayaz14@gmail.com>
2022-06-28 16:40:36 +0500

    contact page made responsive

--- src/components/ContactLeft.vue
+++ src/components/ContactLeft.vue
@@ -1,53 +1,75 @@
 <template>
-    <v-container>
-        <v-row>
-            <v-col class="col-md-12">
-                <h2 class="contact-details-heading mb-4">Don't be Shy !</h2>
-                <p class="body-1">Feel free to get in touch with me. I am always open to discussing new projects, creative ideas or opportunities to be part of your visions.</p>
-            </v-col>
-            
-            <v-col class="col-md-12" style="display: flex" v-for="contact in contactDetails" :key="contact.id">
-                <v-col class="col-md-2">
-                    <v-icon color="secondary" large> {{ contact.icon }} </v-icon>
-                </v-col>
-                <v-col class="col-md-10 ">
-                    <p class="text--secondary text-uppercase mb-2"> {{contact.title}} </p>
-                    <span class="text font-weight-medium"> {{ contact.value }} </span>
-                </v-col>
-            </v-col>   
-            
-        </v-row>
-    </v-container>
+  <v-container>
+    <v-row>
+      <div class="col-md-12">
+        <h2 class="contact-details-heading mb-4">Don't be Shy !</h2>
+        <p class="body-1">
+          Feel free to get in touch with me. I am always open to discussing new
+          projects, creative ideas or opportunities to be part of your visions.
+        </p>
+      </div>
+
+      <div
+        class="col-md-12"
+        style="display: flex"
+        v-for="contact in contactDetails"
+        :key="contact.id"
+      >
+        <div class="col-md-2 contact-details-icon">
+          <v-icon color="secondary" large> {{ contact.icon }} </v-icon>
+        </div>
+        <div class="col-md-10 contact-details-text">
+          <p class="text--secondary text-uppercase mb-2">{{ contact.title }}</p>
+          <span class="text font-weight-medium"> {{ contact.value }} </span>
+        </div>
+      </div>
+    </v-row>
+  </v-container>
 </template>
 
-
 <script>
-    import { mdiMap,mdiEmailOpenOutline,mdiPhone  } from '@mdi/js'
-export default {
-    name: 'ContactLeft',
-    data(){
-        return{
-            contactDetails: [
-                {
-                    id: 0,icon: mdiMap ,title: 'Address Point', value: 'Malir Colony H-Area H5/8 Karachi, Pakistan'
-                },
-                {
-                    id: 1,icon: mdiEmailOpenOutline ,title: 'Mail Me', value: 'inshal.ayaz14@gmail.com'
-                },
-                {
-                    id: 2,icon: mdiPhone ,title: 'Call Me', value: '+92-316-2969299'
-                }
-            ],
-        }
+  import { mdiMap, mdiEmailOpenOutline, mdiPhone } from "@mdi/js";
+  export default {
+    name: "ContactLeft",
+    data() {
+      return {
+        contactDetails: [
+          {
+            id: 0,
+            icon: mdiMap,
+            title: "Address Point",
+            value: "Malir Colony H-Area H5/8 Karachi, Pakistan",
+          },
+          {
+            id: 1,
+            icon: mdiEmailOpenOutline,
+            title: "Mail Me",
+            value: "inshal.ayaz14@gmail.com",
+          },
+          {
+            id: 2,
+            icon: mdiPhone,
+            title: "Call Me",
+            value: "+92-316-2969299",
+          },
+        ],
+      };
     },
-}
+  };
 </script>
 
 <style>
-    .contact-details-heading{
-        font-family: 'Poppins', sans-serif;
-        font-size: 24px;
-        font-weight: 600;
-        text-transform: uppercase;
+  .contact-details-heading {
+    font-family: "Poppins", sans-serif;
+    font-size: 24px;
+    font-weight: 600;
+    text-transform: uppercase;
+  }
+
+  @media (max-width: 680px) {
+    .contact-details-text,
+    .contact-details-icon {
+      text-align: center;
     }
+  }
 </style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,41 +1,36 @@
 <template>
-    
-    <v-container class="contact">
-        <v-row>
-            <v-col class="col-md-12">
-            <PageTitle :heading="'Get in'" :span=" 'Touch' " :title="'contact'"></PageTitle>
-            </v-col>
+  <v-container class="contact">
+    <v-row>
+      <v-col class="col-md-12">
+        <PageTitle
+          :heading="'Get in'"
+          :span="'Touch'"
+          :title="'contact'"
+        ></PageTitle>
+      </v-col>
 
-            <v-col class="col-md-5">
-                <ContactLeft></ContactLeft>
-            </v-col>
-            <v-col class="col-md-7">
-                <ContactRight></ContactRight>
-            </v-col>
-
-
-
-        </v-row>
-    </v-container>
-
-
+      <v-col class="col-md-5 col-sm-12">
+        <ContactLeft></ContactLeft>
+      </v-col>
+      <v-col class="col-md-7 col-sm-12">
+        <ContactRight></ContactRight>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import ContactLeft from '../components/ContactLeft.vue';
-import ContactRight from '../components/ContactRight.vue';
-import PageTitle from '../components/PageTitle.vue';
-export default {
-    name: 'contact',
+  import ContactLeft from "../components/ContactLeft.vue";
+  import ContactRight from "../components/ContactRight.vue";
+  import PageTitle from "../components/PageTitle.vue";
+  export default {
+    name: "contact",
     components: {
-    ContactLeft,
-    ContactRight,
-    PageTitle
-}
-}
+      ContactLeft,
+      ContactRight,
+      PageTitle,
+    },
+  };
 </script>
 
-
-<style>
-   
-</style>
+<style></style>
